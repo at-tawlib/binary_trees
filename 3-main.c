9dd3b0affa026330546c49fdf13dc055733f894d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-01 10:17:50 +0000

    Create 3-binary_tree_delete.c and its test

---
+++ 3-main.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+
+/**
+ * main - test for 3-binary_tree_delete.c
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_right(root, 128);
+	binary_tree_print(root);
+	binary_tree_delete(root);
+	return (0);
+}
