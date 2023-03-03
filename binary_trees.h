3b7a08f5826856f6d3233859058e387cf1b2cceb
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-03 04:19:22 +0000

    Add binary_tree_sibling

--- binary_trees.h
+++ binary_trees.h
@@ -41,5 +41,6 @@ size_t binary_tree_height_2(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int _pow_recursion(int x, int y);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
